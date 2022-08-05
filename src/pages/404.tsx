9ec1e1dea059e49291e21de18dedf390dfe37a19
Muhammad Rivki <muhammad.rivki@live.com>
2022-08-05 15:55:53 +0700

    chore: remove unused files and adjust config

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,8 @@
-import * as React from 'react';
-import { RiAlarmWarningFill } from 'react-icons/ri';
+import * as React from 'react'
+import { RiAlarmWarningFill } from 'react-icons/ri'
 
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import Seo from '@/components/Seo';
+import Layout from '@/components/layout/Layout'
+import Seo from '@/components/Seo'
 
 export default function NotFoundPage() {
   return (
@@ -18,12 +17,9 @@ export default function NotFoundPage() {
               className='drop-shadow-glow animate-flicker text-red-500'
             />
             <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
-            <ArrowLink className='mt-4 md:text-lg' href='/'>
-              Back to Home
-            </ArrowLink>
           </div>
         </section>
       </main>
     </Layout>
-  );
+  )
 }
