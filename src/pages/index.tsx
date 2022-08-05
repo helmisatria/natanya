202893cc64137b6cab2c8b3a52689322f76ee4e5
Muhammad Rivki <muhammad.rivki@live.com>
2022-08-05 18:04:43 +0700

    feat: add mantine components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Anchor, Text, Title } from '@mantine/core'
 import * as React from 'react'
 
 import Layout from '@/components/layout/Layout'
@@ -8,6 +9,28 @@ export default function HomePage() {
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
+      <Title align='center' mt={100}>
+        Welcome to{' '}
+        <Text inherit variant='gradient' component='span'>
+          Natanya
+        </Text>
+      </Title>
+      <Text
+        color='dimmed'
+        align='center'
+        size='lg'
+        sx={{ maxWidth: 580 }}
+        mx='auto'
+        mt='xl'
+      >
+        This starter Next.js project includes a minimal setup for server side
+        rendering, if you want to learn more on Natanya + Next.js integration
+        follow{' '}
+        <Anchor href='https://mantine.dev/guides/next/' size='lg'>
+          this guide
+        </Anchor>
+        . To get started edit index.tsx file.
+      </Text>
     </Layout>
   )
 }
