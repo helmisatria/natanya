d65d8f8e2189aa6b7416523f9b3267c14960a218
Helmi Satria <satriahelmi@gmail.com>
2022-08-06 22:51:00 +0700

    feat: firebase-admin integration to fetch on server side

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,6 +1,6 @@
 export const isProd = process.env.NODE_ENV === 'production'
 export const isLocal = process.env.NODE_ENV === 'development'
 
-export const showLogger = isLocal
-  ? true
-  : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' ?? false
+export const apiUrl = process.env.NEXT_PUBLIC_API_HOST
+
+export const showLogger = isLocal ? true : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' ?? false
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,14 @@ import Seo from '@/components/Seo'
 export default function HomePage() {
   const router = useRouter()
 
+  const [eventCode, setEventCode] = React.useState('')
+
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
       <div className='container mx-auto -mt-12 flex min-h-screen flex-col items-center justify-center'>
-        <Title
-          align='center'
-          className='px-4 font-primary text-7xl tracking-tight md:text-7xl lg:text-8xl'
-        >
+        <Title align='center' className='px-4 font-primary text-7xl tracking-tight md:text-7xl lg:text-8xl'>
           Joining{' '}
           <Text inherit variant='gradient' component='span'>
             polling
@@ -27,18 +26,15 @@ export default function HomePage() {
         <form
           onSubmit={(e) => {
             e.preventDefault()
-            router.push('/event/123')
+            router.push(`/event/${eventCode}`)
           }}
           className='mt-12 flex w-full flex-col justify-center px-4 sm:flex-row'
         >
           <Input
+            required
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEventCode(e.target.value)}
             icon={
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-              >
+              <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' viewBox='0 0 20 20' fill='currentColor'>
                 <path
                   fillRule='evenodd'
                   d='M9.243 3.03a1 1 0 01.727 1.213L9.53 6h2.94l.56-2.243a1 1 0 111.94.486L14.53 6H17a1 1 0 110 2h-2.97l-1 4H15a1 1 0 110 2h-2.47l-.56 2.242a1 1 0 11-1.94-.485L10.47 14H7.53l-.56 2.242a1 1 0 11-1.94-.485L5.47 14H3a1 1 0 110-2h2.97l1-4H5a1 1 0 110-2h2.47l.56-2.243a1 1 0 011.213-.727zM9.03 8l-1 4h2.938l1-4H9.031z'
@@ -50,13 +46,7 @@ export default function HomePage() {
             size='xl'
             autoFocus
           />
-          <Button
-            type='submit'
-            size='xl'
-            color='#fff'
-            variant='default'
-            className='mt-2 bg-primary-50 sm:mt-0 sm:ml-4'
-          >
+          <Button type='submit' size='xl' color='#fff' variant='default' className='mt-2 bg-primary-50 sm:mt-0 sm:ml-4'>
             Submit
           </Button>
         </form>
