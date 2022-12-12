62697fd28d234976cdd725148d52edef5573d7ba
Helmi Satria <satriahelmi@gmail.com>
2022-12-13 00:58:14 +0700

    style: add overlay on slide-over

--- src/components/pages/admin/dashboard/DialogCreateEvent.tsx
+++ src/components/pages/admin/dashboard/DialogCreateEvent.tsx
@@ -61,8 +61,18 @@ export default function DialogCreateEvent({
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as='div' className='relative z-10' onClose={onClose}>
-        <div className='fixed inset-0' />
-
+        <Transition.Child
+          as={Fragment}
+          enter='ease-in-out duration-500'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in-out duration-500'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+        </Transition.Child>
+        ``
         <div className='fixed inset-0 overflow-hidden'>
           <div className='absolute inset-0 overflow-hidden'>
             <div className='pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16'>
