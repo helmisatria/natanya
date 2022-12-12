ff55f0a82de9069fec12da2f762239999d260646
Helmi Satria <satriahelmi@gmail.com>
2022-12-13 00:58:51 +0700

    chore: remove unused code

--- src/components/pages/admin/dashboard/DialogCreateEvent.tsx
+++ src/components/pages/admin/dashboard/DialogCreateEvent.tsx
@@ -72,7 +72,7 @@ export default function DialogCreateEvent({
         >
           <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
         </Transition.Child>
-        ``
+
         <div className='fixed inset-0 overflow-hidden'>
           <div className='absolute inset-0 overflow-hidden'>
             <div className='pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16'>
